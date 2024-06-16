2ab6a54005a7a6e63cbce873e3381c539d49df3b
taufanp <taufanprihantoro15@gmail.com>
2024-06-16 12:46:39 +0700

    feat(comp): add NameInfoForm component

---
+++ src/components/organisms/NameInfoForm/index.tsx
@@ -0,0 +1,101 @@
+import {forwardRef, useImperativeHandle} from 'react';
+import {Controller, useFormContext} from 'react-hook-form';
+import {View} from 'react-native';
+import Toast from 'react-native-toast-message';
+import nameInfo from '../../../constants/components/forms/nameInfo';
+import spaces from '../../../constants/spaces';
+import {FormNameInfo} from '../../../types/components/form';
+import {InputField} from '../../molecules';
+
+export interface NameInfoRef {
+  nextStep: () => void;
+}
+
+interface Props {
+  onValid: () => void;
+}
+
+const NameInfoForm = forwardRef<NameInfoRef, Props>((props, ref) => {
+  const {
+    control,
+    setFocus,
+    formState: {errors},
+    handleSubmit,
+  } = useFormContext<FormNameInfo>();
+
+  useImperativeHandle(ref, () => ({
+    nextStep,
+  }));
+
+  function nextStep() {
+    handleSubmit(
+      () => {
+        props.onValid();
+      },
+      () =>
+        Toast.show({
+          type: 'error',
+          text1: 'Please recheck your form',
+          position: 'bottom',
+        }),
+    )();
+  }
+
+  return (
+    <View style={{gap: spaces.m}}>
+      <Controller
+        control={control}
+        rules={nameInfo.first_name.rule}
+        render={({field: {onChange, onBlur, value, ref}}) => (
+          <InputField
+            error={errors.first_name?.message}
+            isError={!!errors.first_name}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            onSubmitEditing={() => setFocus('last_name')}
+            ref={ref}
+            value={value}
+            {...nameInfo.first_name.input}
+          />
+        )}
+        name="first_name"
+      />
+      <Controller
+        control={control}
+        rules={nameInfo.last_name.rule}
+        render={({field: {onChange, onBlur, value, ref}}) => (
+          <InputField
+            error={errors.last_name?.message}
+            isError={!!errors.last_name}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            onSubmitEditing={() => setFocus('company_name')}
+            ref={ref}
+            value={value}
+            {...nameInfo.last_name.input}
+          />
+        )}
+        name="last_name"
+      />
+      <Controller
+        control={control}
+        rules={nameInfo.company_name.rule}
+        render={({field: {onChange, onBlur, value, ref}}) => (
+          <InputField
+            error={errors.company_name?.message}
+            isError={!!errors.company_name}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            onSubmitEditing={nextStep}
+            ref={ref}
+            value={value}
+            {...nameInfo.company_name.input}
+          />
+        )}
+        name="company_name"
+      />
+    </View>
+  );
+});
+
+export default NameInfoForm;
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,5 +1,6 @@
 export {default as Header} from './Header';
 export {default as LoginFooter} from './LoginFooter';
 export {default as MultiStepProgress} from './MultiStepProgress';
+export {default as NameInfoForm} from './NameInfoForm';
 export {default as Screen} from './Screen';
 export {default as StateWrapper} from './StateWrapper';
