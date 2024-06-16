5f539b4c20bbf7c1499bd1f4b77039a905953707
taufanp <taufanprihantoro15@gmail.com>
2024-06-16 10:06:42 +0700

    feat(comp): add RowIcon component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,5 +4,6 @@ export {default as ButtonIcon} from './ButtonIcon';
 export {default as EmployeeTile} from './EmployeeTile';
 export {default as InputField} from './InputField';
 export {default as LinkRow} from './LinkRow';
-export {default as QuickAction} from './QuickAction';
 export {default as PhoneQuickActions} from './PhoneQuickActions';
+export {default as QuickAction} from './QuickAction';
+export {default as RowIcon} from './RowIcon';
