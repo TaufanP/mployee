3821fcfa28c1e389ee325ca3cf1734843c214ba0
taufanp <taufanprihantoro15@gmail.com>
2024-06-16 09:23:08 +0700

    feat(helpers): add helper function to open another app

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,5 @@ export {default as isTokenExpired} from './isTokenExpired';
 export {default as joinFullAddress} from './joinFullAddress';
 export {default as joinString} from './joinString';
 export {default as logger} from './logger';
+export {default as openApps} from './openApps';
 export {default as queryKeys} from './queryKeys';
-export {default as joinString} from './joinString';
---
+++ src/helpers/openApps/index.ts
@@ -0,0 +1,50 @@
+import {Linking} from 'react-native';
+import Toast from 'react-native-toast-message';
+
+export default async function openApps(
+  type: 'web' | 'phone' | 'mail',
+  payload?: string,
+) {
+  if (!!payload) {
+    let link = '';
+    switch (type) {
+      case 'phone':
+        link = `tel:${payload}`;
+        break;
+      case 'web':
+        link = payload;
+        break;
+      case 'mail':
+        link = `mailto:${payload}`;
+        break;
+
+      default:
+        break;
+    }
+    try {
+      if (type === 'web') {
+        const canOpen = await Linking.canOpenURL(link);
+        if (canOpen) Linking.openURL(link);
+        else {
+          Toast.show({
+            position: 'bottom',
+            type: 'error',
+            text1: 'Cannot visit the site!',
+          });
+        }
+      } else Linking.openURL(link);
+    } catch (error) {
+      Toast.show({
+        position: 'bottom',
+        type: 'error',
+        text1: 'Cannot do the action!',
+      });
+    }
+  } else {
+    Toast.show({
+      position: 'bottom',
+      type: 'error',
+      text1: 'Cannot do the action!',
+    });
+  }
+}
